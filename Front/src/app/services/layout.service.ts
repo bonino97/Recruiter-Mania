93b91711b1a1b423def982dfd6fe1d1416da8b55
bonino97 <juanbonino97@gmail.com>
2020-07-22 00:54:06 -0300

    Add Services

--- Front/src/app/services/layout.service.ts
+++ Front/src/app/services/layout.service.ts
@@ -12,19 +12,23 @@ export class LayoutService {
 
   constructor(
     private http: HttpClient,
-    private router: Router
+    private router: Router,
   ) { }
 
-  GetPageOptions():Observable<any>{
+  GetPageOptions():Observable<any> {
     return this.http.get(environment.url);
   }
 
-  GetJobs(page, limit): Observable<any>{
+  GetJobs(page, limit): Observable<any> {
     return this.http.get(`${environment.url}/jobs?page=${page}&limit=${limit}`);
   }
 
-  GetNewJobOptions():Observable<any>{
+  GetNewJobOptions():Observable<any> {
     return this.http.get(`${environment.url}/job/new`);
+  }
+
+  GetOneJob(url):Observable<any> { 
+    return this.http.get(`${environment.url}/job/${url}`);
   }
 
   PostNewJob(job:Job):Observable<any>{
@@ -33,4 +37,13 @@ export class LayoutService {
     
     return this.http.post(`${environment.url}/job/new`, params, {headers: headers});
   }
+
+  UpdateJob(job:Job):Observable<any>{
+    let params = JSON.stringify(job);
+    let headers = new HttpHeaders().set('Content-Type','application/json');
+    
+    return this.http.put(`${environment.url}/job/${job.Url}`, params, {headers: headers});
+  }
+
+
 }

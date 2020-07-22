788da273de478c28a5c07304bbe9ef966f70845c
bonino97 <juanbonino97@gmail.com>
2020-07-22 00:52:41 -0300

    Add Job Component

--- Front/src/app/pages/job/job.component.ts
+++ Front/src/app/pages/job/job.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LayoutService } from 'src/app/services/layout.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-job',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JobComponent implements OnInit {
 
-  constructor() { }
+  job:any;
+  skills:any;
+
+  constructor(
+    private _LayoutService: LayoutService,
+    private route: ActivatedRoute,
+    private router: Router
+  ){}
 
   ngOnInit(): void {
+    this.job = [];
+    this.route.params.subscribe(params => { //Toma el valor job.url desde la URL
+      this._LayoutService.GetOneJob(params['url'])
+      .subscribe((data:any) => {
+        this.job = data;
+        this.skills = data.Skills;
+      }, (error: any) => {
+        console.error(error);
+      });
+   })
+  }
+
+  editJob(job){
+    this.router.navigate([`/jobs/job/${job.Url}/edit`]);
   }
 
 }

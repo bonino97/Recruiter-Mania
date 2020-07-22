63a2afafd44ed2a5f79b716f8f6a7c0e651cd2bd
bonino97 <juanbonino97@gmail.com>
2020-07-22 00:50:32 -0300

    Add Routes

--- API/Routes/Index.js
+++ API/Routes/Index.js
@@ -10,12 +10,17 @@ module.exports = () => {
     router.get('/api', HomeController.HomeOptions);
     
     // Banner Formulario - Nuevo Trabajo
-    router.get('/api/job/new', JobsController.NewJobOptions);
+    // router.get('/api/job/new', JobsController.NewJobOptions);
     
     router.get('/api/jobs', JobsController.GetJobs);
+    router.get('/api/job/:url', JobsController.GetJobByUrl);
+    
 
     // Cargar Nuevo Trabajo o Vacante.
     router.post('/api/job/new', JobsController.NewJob);
+
+    //Actualizar Trabajo
+    router.put('/api/job/:url', JobsController.UpdateJob)
 
     return router;
 }

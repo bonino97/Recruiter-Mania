5e8a0d7012925af55e469722cb9062e661f230da
bonino97 <juanbonino97@gmail.com>
2020-07-23 18:31:05 -0300

    Add Register Endpoint

--- API/Controllers/UsersController.js
+++ API/Controllers/UsersController.js
@@ -1,5 +1,27 @@
+const User = require('../Models/User');
 
+exports.Register = async (req,res,next) => {
 
-exports.Register = async (req,res) => {
-    
+    try {
+        const user = new User(req.body);
+        const newUser = await user.save();
+        if(!newUser) return next();
+        return res.status(200).json({
+            success: true,
+            msg: 'Your account was created successfully!'
+        });
+    } catch (e) {
+
+        if(e.code === 11000) {
+            return res.status(400).json({
+                success: false,
+                msg: 'Hey, this email is in use!'
+            });
+        }
+
+        return res.status(400).json({
+            success: false,
+            msg: 'An error ocurred. Please, try again later.'
+        });
+    }
 }

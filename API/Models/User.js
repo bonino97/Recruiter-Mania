d00d102f226df57b5f6e312329a0ab82d419e80a
bonino97 <juanbonino97@gmail.com>
2020-07-23 18:31:39 -0300

    Update User Model

---
+++ API/Models/User.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const bcrypt = require('bcrypt');
+
+const UserSchema = new mongoose.Schema({ 
+    Email:{
+        type: String,
+        required: 'The Email is required.',
+        trim: true,
+        unique: true,
+        lowercase: true
+    },
+    FullName:{
+        type: String,
+        required: 'The Full Name is required.'
+    },
+    Password:{
+        type: String, 
+        required: 'The Password is required.',
+        trim: true
+    },
+    Token: String,
+    Expires: Date
+});
+
+//Metodo para Hashear los Passwords
+UserSchema.pre('save', async function (next) {
+    //Si el password ya esta hasheado, no hacemos nada.
+    if(!this.isModified('Password')) {
+        return next(); //Detener la ejecucion, y continuar con el siguiente middleware
+    }
+    const Hash = await bcrypt.hash(this.Password, 10);
+    this.Password = Hash; 
+
+    next();
+});
+
+UserSchema.post('save', function (error, doc, next) {
+
+})
+
+
+module.exports = mongoose.model('User', UserSchema);

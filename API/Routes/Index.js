6709947b7f64249aeded5177d719637cfd7ee412
bonino97 <juanbonino97@gmail.com>
2020-07-23 18:31:59 -0300

    Add Register URL

--- API/Routes/Index.js
+++ API/Routes/Index.js
@@ -1,35 +1,43 @@
 const express = require('express');
 const router = express.Router();
 
+
+//Default API Url
+const apiUrl = '/api'
+
 //Controllers 
 const HomeController = require('../Controllers/HomeController');
 const JobsController = require('../Controllers/JobsController');
+const UsersController = require('../Controllers/UsersController');
+const ValidationsController = require('../Controllers/ValidationsController');
 
 module.exports = () => {
     /********************/
     /*  HomeController  */
     /********************/
     
-    router.get('/api', HomeController.HomeOptions);
+    router.get(apiUrl, HomeController.HomeOptions);
     
     /********************/
     /*  JobsController  */
     /********************/
     /* GET */
-    router.get('/api/jobs', JobsController.GetJobs);
-    router.get('/api/job/:url', JobsController.GetJobByUrl);
+    router.get(`${apiUrl}/jobs`, JobsController.GetJobs);
+    router.get(`${apiUrl}/job/:url`, JobsController.GetJobByUrl);
     
     /* POST */
     // Cargar Nuevo Trabajo o Vacante.
-    router.post('/api/job/new', JobsController.NewJob);
+    router.post(`${apiUrl}/job/new`, JobsController.NewJob);
 
     /* PUT */
     //Actualizar Trabajo
-    router.put('/api/job/:url', JobsController.UpdateJob)
+    router.put(`${apiUrl}/job/:url`, JobsController.UpdateJob);
     
     /*******************/
     /* UsersController */
     /*******************/
+
+    router.post(`${apiUrl}/register`, ValidationsController.SanitizeRegisterData, UsersController.Register);
     
 
     return router;

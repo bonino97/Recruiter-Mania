9fbef61e6bb6a9f33312962be785e8c746d2d7e8
bonino97 <juanbonino97@gmail.com>
2020-07-23 18:33:23 -0300

    Update Register Alerts

--- Front/src/app/pages/register/register.component.ts
+++ Front/src/app/pages/register/register.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, OnDestroy} from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { MustMatch } from "./password-validator.component";
+import { LayoutService } from 'src/app/services/layout.service';
+import { User } from 'src/app/models/user.model';
+import swal from "sweetalert2";
 
 @Component({
   selector: 'app-register',
@@ -22,7 +25,10 @@ export class RegisterComponent implements OnInit, OnDestroy {
   public registerForm: FormGroup;
   public register = false;
 
-  constructor(public formBuilder: FormBuilder) {}
+  constructor(
+    public formBuilder: FormBuilder,
+    private _LayoutService: LayoutService
+  ) {}
 
   ngOnInit() {
     var body = document.getElementsByTagName("body")[0];
@@ -52,7 +58,40 @@ export class RegisterComponent implements OnInit, OnDestroy {
   }
 
   onSubmit(){
-    console.log(this.registerForm.valid);
     if(this.registerForm.invalid) return;
+    const user = new User(this.registerForm.value.fullName, this.registerForm.value.email, this.registerForm.value.password);
+    this._LayoutService.Register(user).subscribe(
+      (data:any) => {
+
+        if(data.success){
+          swal.fire({
+            html: `<span style='color:grey'> ${data.msg} <span>`,
+            buttonsStyling: false,
+            confirmButtonClass: "btn btn-danger btn-simple",
+            background: '#ffffff'
+          });
+        }
+
+      }, (error: any) => {
+        if(!!error.error.msg){
+          swal.fire({
+            html: `<span style='color:grey'> ${error.error.msg} <span>`,
+            buttonsStyling: false,
+            confirmButtonClass: "btn btn-danger btn-simple",
+            background: '#ffffff'
+          });
+        }
+
+        if(!!error.error.errors){
+          if(error.error.errors.length > 0){
+            swal.fire({
+              html: `<span style='color:grey'> ${error.error.errors} <span>`,
+              buttonsStyling: false,
+              confirmButtonClass: "btn btn-danger btn-simple",
+              background: '#ffffff'
+            });
+          }
+        }
+      });
   }
 }

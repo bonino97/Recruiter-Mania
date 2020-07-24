4881b48eb59a6777b1f6085a67d2e34de0ae541d
bonino97 <juanbonino97@gmail.com>
2020-07-24 19:52:06 -0300

    Add PanelOptions Method

--- API/Controllers/HomeController.js
+++ API/Controllers/HomeController.js
@@ -3,8 +3,20 @@ const Job = require('../Models/Job');
 exports.HomeOptions = async (req, res, next) => { //Mostrar Trabajos en el Curso
 
     res.status(200).json({
-        tagLine: 'Find, Share & Post Developers Jobs!',
+        tagLine: 'Find, Share & Post your Job position!',
         bar: true,
-        postJobButton: true
+        postJobButton: true,
+        home: true
+    });
+}
+
+exports.PanelOptions = async (req, res, next) => { //Mostrar Trabajos en el Curso
+
+    res.status(200).json({
+        tagLine: 'Admin Panel',
+        subTagLine: 'Manage your Jobs position from here...',
+        postJobButton: true,
+        editProfileButton: true,
+        panel: true
     });
 }

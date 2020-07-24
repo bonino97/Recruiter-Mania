c0451b17b6b230475935b03e199d95ca1fdee1c9
bonino97 <juanbonino97@gmail.com>
2020-07-24 19:53:46 -0300

    Add Input Functionality to Header

--- Front/src/app/components/header/header.component.ts
+++ Front/src/app/components/header/header.component.ts
@@ -11,18 +11,41 @@ export class HeaderComponent implements OnInit {
   tagLine: any;
   subTagLine: any;
   jobButton: any;
+  editButton: any;
+  panel: any;
   newJobPath = "/jobs/new-job";
+
+  @Input() getPath: string;
 
   constructor(
     public _LayoutService: LayoutService
   ) { }
 
   ngOnInit(): void {
-    this._LayoutService.GetPageOptions()
-    .subscribe((data:any) => {
-      this.tagLine = data.tagLine; //Llena el texto del Banner
-      this.jobButton = data.postJobButton; //Habilita/Deshabilita Boton de Nuevo Trabajo.
-    });
+
+    switch(this.getPath){
+      case 'Home': {
+        this._LayoutService.GetPageOptions()
+        .subscribe((data:any) => {
+          this.tagLine = data.tagLine; //Llena el texto del Banner
+          this.jobButton = data.postJobButton; //Habilita/Deshabilita Boton de Nuevo Trabajo.
+        });
+        break;
+      }
+      case 'Panel': {
+        this._LayoutService.GetPanelOptions()
+        .subscribe((data:any) => {
+          this.tagLine = data.tagLine; //Llena el texto del Banner
+          this.subTagLine = data.subTagLine;
+          this.jobButton = data.postJobButton; //Habilita/Deshabilita Boton de Nuevo Trabajo.
+          this.editButton = data.editProfileButton; //Habilita/Deshabilita Boton de Nuevo Trabajo.
+          this.panel = data.panel;
+        });
+        break;
+      }
+    }
+
+
   }
 
 }

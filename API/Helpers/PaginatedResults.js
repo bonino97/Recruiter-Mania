e82546c285ad747d3e979999961b51a6637a54d6
bonino97 <juanbonino97@gmail.com>
2020-07-25 01:40:39 -0300

    Add Method to Paginate Jobs by id

--- API/Helpers/PaginatedResults.js
+++ API/Helpers/PaginatedResults.js
@@ -27,3 +27,33 @@ exports.PaginatedResults = async (model, page, limit) => {
         throw e;
     }
 }
+
+exports.PaginatedResultsById = async (model, queryFilter, page, limit) => {
+    try{
+        const startIndex = (page - 1) * limit;
+        const endIndex = page * limit;
+        
+        const results = {}
+
+        if(endIndex < await model.find(queryFilter).countDocuments().exec()) {
+            results.nextPage = {
+                page: page+1,
+                limit: limit
+            }
+        }
+
+        if(startIndex > 0){
+            results.previousPage = {
+                page: page-1,
+                limit: limit
+            }
+        }
+        results.totalPages = Math.ceil(await model.find(queryFilter).countDocuments().exec()/limit);
+        results.actualPage = page;
+        results.results = await model.find(queryFilter).sort({_id:-1}).limit(limit).skip(startIndex).exec();
+        return results;
+
+    } catch (e){
+        throw e;
+    }
+}

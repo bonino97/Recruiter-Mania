77cdecc6e39b01158c9ec3a84b2f927aae7fefbf
bonino97 <juanbonino97@gmail.com>
2020-07-25 17:44:16 -0300

    Update UsersController

--- API/Controllers/UsersController.js
+++ API/Controllers/UsersController.js
@@ -1,4 +1,5 @@
 const User = require('../Models/User');
+const { update } = require('../Models/User');
 
 exports.Register = async (req,res,next) => {
 
@@ -11,7 +12,7 @@ exports.Register = async (req,res,next) => {
             msg: 'Your account was created successfully!'
         });
     } catch (e) {
-
+        console.error(e);
         if(e.code === 11000) {
             return res.status(400).json({
                 success: false,
@@ -24,4 +25,37 @@ exports.Register = async (req,res,next) => {
             msg: 'An error ocurred. Please, try again later.'
         });
     }
+}
+
+exports.GetMyInstance = async (req,res) => {
+    try {
+
+        const user = await User.findById(req.user._id);
+        return res.status(200).json({
+            success: true,
+            data: user
+        });
+
+    } catch (e) {
+        console.error(e)
+        return res.status(400).json({
+            success: false,
+            msg: 'An error ocurred. Please, try again later.'
+        });
+    }
+}
+
+exports.UpdateProfile = async (req,res) => {
+    try{
+        const updatedUser = req.body;
+        const user = await User.findOneAndUpdate({_id: req.user._id}, updatedUser, {new: true, runValidators: true});
+        return res.status(200).json({
+            success: true,
+            msg: 'User successfully updated!',
+            data: user
+        });
+    }catch(e){
+        console.error(e);
+    }
+
 }

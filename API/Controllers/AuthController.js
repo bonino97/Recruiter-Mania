e5fd5487317d3c0d38ba76b6ac758da732cf2d5a
bonino97 <juanbonino97@gmail.com>
2020-07-25 01:39:36 -0300

    Update Res in Login method

--- API/Controllers/AuthController.js
+++ API/Controllers/AuthController.js
@@ -8,7 +8,11 @@ exports.AuthenticateUser = async (req,res,next) => {
         if(!user) return res.status(400).json({"success" : false, "message" : info});
         req.login(user, function(error) {
             if (error) return res.status(400).json({"success" : false, "message" : error});
-            next();
+            return res.status(200).json({
+                success: true,
+                lemonCookie: res.req.headers.cookie.split('=')[1],
+                msg: 'Logging...'
+            });
         });
     })(req, res, next);
 

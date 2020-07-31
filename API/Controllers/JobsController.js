bce7cac9bc4ee658ad39b8e207080207c1ff22bf
bonino97 <juanbonino97@gmail.com>
2020-07-31 00:04:30 -0300

    Add Resume & Candidate Logic

--- API/Controllers/JobsController.js
+++ API/Controllers/JobsController.js
@@ -1,5 +1,8 @@
 const Job = require('../Models/Job');
 const { PaginatedResults, PaginatedResultsById } = require('../Helpers/PaginatedResults');
+const multer = require('multer');
+const shortid = require('shortid');
+const e = require('express');
 
 exports.GetJobs = async (req,res) => {
     try {
@@ -41,6 +44,7 @@ exports.NewJob = async (req,res) => {
 
 exports.UpdateJob = async (req,res) => {
     try{
+
         const updatedJob = req.body;
         const job = await Job.findOneAndUpdate(
             {Url: req.params.url},
@@ -48,7 +52,12 @@ exports.UpdateJob = async (req,res) => {
             {new: true, runValidators: true}
         );
         
-        return res.status(200).json({job});
+        return res.status(200).json({
+            success: true,
+            data: job,
+            message: 'Job Updated successfully...'
+        });
+
     } catch (e) {
         console.error(e);
         return res.status(400).json({message: e.message});
@@ -66,7 +75,9 @@ exports.GetJobsByUserId = async (req,res) => {
 
     } catch (e) {
         console.error(e);
-        return res.status(400).json({message: e.message});
+        return res.status(400).json({
+            message: e.message
+        });
     }
 }
 
@@ -92,4 +103,132 @@ const VerifyUser = (job = {}, user = {}) => {
         return false;
     }
     return true;
+}
+
+const multerConfig = {
+    limits: { fileSize: 2000000 }, // 2 mb,
+    storage: filerStorage = multer.diskStorage({
+        destination: (req,file,cb) => {
+            cb(null, __dirname+'../../Uploads/Resumes');
+        }, 
+        filename: (req,file,cb) => {
+            const extension = file.mimetype.split('/')[1];
+            cb(null, `${shortid.generate()}.${extension}`);
+        }
+    }),
+    fileFilter(req,file,cb) {
+        if(file.mimetype === 'application/pdf') {
+            // el cb se ejecuta como true o false : true cuando la imagen se acepta
+            cb(null, true);
+        } else {
+            
+            cb(new Error('Invalid format, only PDF allowed...'));
+        }
+    }
+}
+
+const Upload = multer(multerConfig).single('Resume');
+
+exports.SendResume = async (req,res,next) => {
+    try {
+        Upload(req,res,function(error){
+            if(error) {
+                console.log(error);
+                if(error instanceof multer.MulterError) {
+                    if(error.code == 'LIMIT_FILE_SIZE') {
+                        
+                        return res.status(400).json({
+                            success: false,
+                            message:'File Exceed 2MB'
+                        });
+
+                    } else {
+                        return res.status(400).json({
+                            success: false,
+                            message: error.message
+                        });
+                    }
+                } else {
+                    return res.status(400).json({
+                        success: false,
+                        message: error.message
+                    });
+                }
+            } else {
+                return next();
+            }
+        });
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+exports.SaveCandidate = async (req,res) => {
+    try{
+
+        const job = await Job.findOne({Url: req.params.url});
+
+        if(!job) {
+            return res.status(400).json({
+                success: false,
+                message: 'Job not found...'
+            });
+        }
+
+        if(job.Candidates == null){
+            job.Candidates = [];
+        }
+
+        let candidates = {
+                FirstName: req.body.FirstName,
+                LastName: req.body.LastName,
+                Email: req.body.Email,
+                LinkedinUrl: req.body.LinkedinUrl,
+                Resume: req.file.filename
+        }
+
+        job.Candidates.push(candidates);
+
+        var jobUpdated = await job.save();
+
+        console.log(jobUpdated);
+
+        return res.status(200).json({
+            success: true,
+            message: 'Resume sended successfully'
+        });
+
+    } catch (e) {
+        console.error(e)
+        return res.status(400).json({
+            success: false,
+            message: 'Something wrong...'
+        });
+    }
+
+}
+
+exports.GetCandidatesPerJob = async (req,res) => {
+    const job = await Job.findOne({Url: req.params.url});
+    
+    if(!job) {
+        return res.status(400).json({
+            success: false,
+            message: 'Job not found...'
+        });
+    }
+    
+    if(job.User == req.user._id.toString()){ //Valido filtrado de informacion. Para q otro no vea las vacantes.
+        
+        return res.status(200).json({
+            success: true,
+            data: job.Candidates
+        });
+
+    } else {
+        return res.status(401).json({
+            success: false,
+            message: 'Unauthorized.'
+        });
+    }
 }

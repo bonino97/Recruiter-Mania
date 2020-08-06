b402aa438e7c12d9fe8100996cd1ac31ae08045f
bonino97 <juanbonino97@gmail.com>
2020-08-06 01:21:33 -0300

    Fix routing

--- Front/src/app/app-routing.module.ts
+++ Front/src/app/app-routing.module.ts
@@ -1,3 +1,6 @@
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { ResetPasswordComponent } from './pages/reset-password/reset-password.component';
+import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
 
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
@@ -5,7 +8,6 @@ import { BrowserModule } from "@angular/platform-browser";
 import { Routes, RouterModule } from "@angular/router";
 
 import { LayoutComponent } from './layouts/layout/layout.component';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 
@@ -28,28 +30,20 @@ const routes: Routes = [
     ]
   },
   {
-    path: "",
-    component: RegisterComponent,
-    data: { animation: 'isRight' },
-    children: [
-      {
-        path: "register",
-        loadChildren:
-          "./layouts/layout/layout.module#LayoutModule"
-      }
-    ]
+    path: "register",
+    component: RegisterComponent
   },
   {
-    path: "",
-    component: LoginComponent,
-    data: { animation: 'isRight' },
-    children: [
-      {
-        path: "login",
-        loadChildren:
-          "./layouts/layout/layout.module#LayoutModule"
-      }
-    ]
+    path: "login",
+    component: LoginComponent
+  },
+  {
+    path: "forgot-password",
+    component: ForgotPasswordComponent
+  },
+  {
+    path: "reset-password/:url",
+    component: ResetPasswordComponent
   },
   {
     path: "**",
